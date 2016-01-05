3249b8173e0143d80553c67a1ccfe6f3f776490c
l0custdev@gmail.com <7JrQG62UCTEeAtunzWYY>
2016-01-05 15:44:55 +0000

    Restructered the project

--- dicionario/dicionario.py
+++
@@ -1,4 +0,0 @@
-fname = 'livros/oslusiadas.txt'
-
-with open(fname) as f:
-    content = f.readlines()
---
+++ src/loadDicionary.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class loadDicionary {
+
+    public static void main(String[] args) throws IOException{
+        File dicionario = new File("dictionary/test.txt");
+
+        if (!dicionario.exists()) return;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(dicionario)));
+
+        while (true) {
+            String str = reader.readLine();
+            if (str == null) {
+                break;
+            } else {
+                String[] aux = str.split("\\W+");
+            }
+        }
+    }
+}
